bdb31f862b2e5e4db9478a4e7b70621c685a528a
童祺俊 <tongqijun@doukishunnoMac-mini.local>
2020-03-10 14:19:13 +0900

    commited

--- Atcoder_editer.cpp
+++ Atcoder_editer.cpp
@@ -1,59 +1,31 @@
 #include <bits/stdc++.h>
 #define all(x) (x).begin(), (x).end()
 typedef long long ll;
-#define MOD 1000000007
+#define MOD 998244353
 using namespace std;
 int main() {
-    ll n, k;
-    cin >> n >> k;
+    int n;
+    cin >> n;
     vector<ll> a(n);
     for(int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    sort(all(a));
-    ll l = LLONG_MIN / 2, r = LLONG_MAX / 2;
+    if(*max_element(all(a)) < n) {
+        cout << 0 << endl;
+        return 0;
+    }
+    ll l = 0, r = LLONG_MAX / 2;
     while(l + 1 < r) {
-        ll tmp = 0, t2 = 0;
-        ll x = (l + r) / 2;
+        ll m = (l + r) / 2, tmp = 0;
         for(int i = 0; i < n; i++) {
-            t2 += (a[i] * a[i] <= x);
-            if(a[i] > 0) {
-                int left = -1, right = n;
-                while(left + 1 < right) {
-                    int m = (left + right) / 2;
-                    if(a[m] * a[i] <= x) {
-                        left = m;
-                    } else {
-                        right = m;
-                    }
-                }
-                tmp += right;
-            }
-            if(a[i] == 0) {
-                if(0 <= x) {
-                    tmp += n;
-                }
-            }
-            if(a[i] < 0) {
-                int left = -1, right = n;
-                while(left + 1 < right) {
-                    int m = (left + right) / 2;
-                    if(a[m] * a[i] <= x) {
-                        right = m;
-                    } else {
-                        left = m;
-                    }
-                }
-                tmp += n - right;
-            }
+            ll x = a[i] + m;
+            tmp += (x + 1) / (n + 1);
         }
-        tmp -= t2;
-        tmp /= 2;
-        if(tmp < k) {
-            l = x;
+        if(tmp <= m) {
+            r = m;
         } else {
-            r = x;
+            l = m;
         }
     }
-    cout << l << endl;
+    cout << r << endl;
 }
