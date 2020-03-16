080b86b2f13e8b8329a148378d57f8666464e1e7
童祺俊 <tongqijun@doukishunnoMac-mini.local>
2020-03-16 11:14:53 +0900

    reflects ignore

--- Atcoder_editer.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#define all(x) (x).begin(), (x).end()
-typedef long long ll;
-#define MOD 1000000007
-using namespace std;
-int main() {
-    long double a, b, c;
-    cin >> a >> b >> c;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# competitive-programming-snippet
-競技プログラミングで用いるアルゴリズムのライブラリ(自分用、他サイトからのコピペとかしてます)
