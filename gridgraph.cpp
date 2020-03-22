de7766aeebe6cc1d5367969b3034b623e70d87e5
童祺俊 <tongqijun@doukishunnoMac-mini.local>
2020-03-22 13:09:45 +0900

    library modefied

--- gridgraph.cpp
+++ gridgraph.cpp
@@ -1,29 +1,39 @@
-vector<vector<pair<ll, ll>>> gridgraph(int h, int w, vector<vector<char>> s)
-{
+//.の点のみを長さ1の辺でつなぐパターン
+vector<vector<pair<ll, ll>>> gridgraph1(int h, int w, vector<string> s) {
     vector<vector<pair<ll, ll>>> edge(h * w);
-    for (int i = 0; i < h; i++)
-    {
-        for (int j = 0; j < w; j++)
-        {
-            if (s[i][j] == '#')
-            {
+    for(int i = 0; i < h; i++) {
+        for(int j = 0; j < w; j++) {
+            if(s[i][j] == '#') {
                 continue;
             }
-            if (i - 1 >= 0 && s[i - 1][j] == '.')
-            {
+            if(i - 1 >= 0 && s[i - 1][j] == '.') {
                 edge[i * w + j].push_back(make_pair(1, (i - 1) * w + j));
             }
-            if (i + 1 < h && s[i + 1][j] == '.')
-            {
+            if(i + 1 < h && s[i + 1][j] == '.') {
                 edge[i * w + j].push_back(make_pair(1, (i + 1) * w + j));
             }
-            if (j + 1 < w && s[i][j + 1] == '.')
-            {
+            if(j + 1 < w && s[i][j + 1] == '.') {
                 edge[i * w + j].push_back(make_pair(1, i * w + j + 1));
             }
-            if (j - 1 >= 0 && s[i][j - 1] == '.')
-            {
+            if(j - 1 >= 0 && s[i][j - 1] == '.') {
                 edge[i * w + j].push_back(make_pair(1, i * w + j - 1));
+            }
+        }
+    }
+    return edge;
+}
+//異なるグリッドを長さ1で繋ぎ、それ以外を0でつなぐパターン
+vector<vector<pair<ll, ll>>> gridgraph2(int h, int w, vector<string> s) {
+    vector<vector<pair<ll, ll>>> edge(h * w);
+    for(int i = 0; i < h; i++) {
+        for(int j = 0; j < w; j++) {
+            if(i + 1 < h) {
+                edge[i * w + j].push_back(
+                    make_pair(s[i][j] != s[i + 1][j], (i + 1) * w + j));
+            }
+            if(j + 1 < w) {
+                edge[i * w + j].push_back(
+                    make_pair(s[i][j] != s[i][j + 1], i * w + j + 1));
             }
         }
     }
