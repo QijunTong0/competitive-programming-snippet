992c0df809c8cad614ada9e97d755a283f1bdbf5
童祺俊 <tongqijun@doukishunnoMac-mini.local>
2020-03-31 16:05:11 +0900

    various gridgraphs added

--- gridgraph.cpp
+++ gridgraph.cpp
@@ -1,3 +1,24 @@
+//全部つなぐパターン重みなし
+vector<vector<int>> gridgraph(int h, int w, vector<string> s) {
+    vector<vector<int>> edge(h * w);
+    for(int i = 0; i < h; i++) {
+        for(int j = 0; j < w; j++) {
+            if(i - 1 >= 0) {
+                edge[i * w + j].push_back((i - 1) * w + j);
+            }
+            if(i + 1 < h) {
+                edge[i * w + j].push_back((i + 1) * w + j);
+            }
+            if(j + 1 < w) {
+                edge[i * w + j].push_back(i * w + j + 1);
+            }
+            if(j - 1 >= 0) {
+                edge[i * w + j].push_back(i * w + j - 1);
+            }
+        }
+    }
+    return edge;
+}
 //.の点のみを長さ1の辺でつなぐパターン
 vector<vector<pair<ll, ll>>> gridgraph1(int h, int w, vector<string> s) {
     vector<vector<pair<ll, ll>>> edge(h * w);
@@ -34,6 +55,30 @@ vector<vector<pair<ll, ll>>> gridgraph2(int h, int w, vector<string> s) {
             if(j + 1 < w) {
                 edge[i * w + j].push_back(
                     make_pair(s[i][j] != s[i][j + 1], i * w + j + 1));
+            }
+        }
+    }
+    return edge;
+}
+//重みなしver
+vector<vector<int>> gridgraph(int h, int w, vector<string> s) {
+    vector<vector<int>> edge(h * w);
+    for(int i = 0; i < h; i++) {
+        for(int j = 0; j < w; j++) {
+            if(s[i][j] == '#') {
+                continue;
+            }
+            if(i - 1 >= 0 && s[i - 1][j] == '.') {
+                edge[i * w + j].push_back((i - 1) * w + j);
+            }
+            if(i + 1 < h && s[i + 1][j] == '.') {
+                edge[i * w + j].push_back((i + 1) * w + j);
+            }
+            if(j + 1 < w && s[i][j + 1] == '.') {
+                edge[i * w + j].push_back(i * w + j + 1);
+            }
+            if(j - 1 >= 0 && s[i][j - 1] == '.') {
+                edge[i * w + j].push_back(i * w + j - 1);
             }
         }
     }
