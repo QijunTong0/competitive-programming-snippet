2314fc6684c5e63e21c084248c1e8db892931e68
童祺俊 <tongqijun@doukishunnoMac-mini.local>
2020-04-05 00:41:00 +0900

    約数系追加

---
+++ 約数素因数.cpp
@@ -0,0 +1,24 @@
+vector<ll> divisor(ll n) {
+    vector<ll> ret;
+    for(ll i = 1; i * i <= n; i++) {
+        if(n % i == 0) {
+            ret.push_back(i);
+            if(i * i != n)
+                ret.push_back(n / i);
+        }
+    }
+    sort(begin(ret), end(ret));
+    return (ret);
+}
+map<ll, ll> prime_factor(ll n) {
+    map<ll, ll> ret;
+    for(ll i = 2; i * i <= n; i++) {
+        while(n % i == 0) {
+            ret[i]++;
+            n /= i;
+        }
+    }
+    if(n != 1)
+        ret[n] = 1;
+    return ret;
+}
